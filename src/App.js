7dd8cb61c091980069ff4457e70b42b807e5bc9e
othea32 <othea32@gmail.com>
2023-06-29 15:07:19 -0700

    updated article.js added images

--- src/App.js
+++ src/App.js
@@ -6,13 +6,13 @@ import Footer from './Footer';
 
 
 
+
 function App() {
   return (
     <div className="App">
       <Header />
       <Nav />
-      <Article title="On the Street in Brooklyn" imageSrc="/src/images/blog-image-1.jpg" content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat, quia quo! Commodi eaque labore omnis cumque reiciendis porro ipsa aliquam delectus expedita a dolorum enim voluptatem tempore similique at nisi eos ipsam saepe, ea quasi magnam. Voluptatibus voluptatum quae, nesciunt assumenda ipsam commodi rerum enim sit, dignissimos doloremque soluta ad deserunt quis quas voluptate reiciendis laboriosam neque maiores dolores, consequatur molestias aliquam. Corporis error cupiditate ex ipsa? Cupiditate eum consequuntur atque illum asperiores enim porro." />
-      <Article title="Vintage in Vogue" imageSrc="/src/images/blog-image-2.jpg" content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat, quia quo! Commodi eaque labore omnis cumque reiciendis porro ipsa aliquam delectus expedita a dolorum enim voluptatem tempore similique at nisi eos ipsam saepe, ea quasi magnam. Voluptatibus voluptatum quae, nesciunt assumenda ipsam commodi rerum enim sit, dignissimos doloremque soluta ad deserunt quis quas voluptate reiciendis laboriosam neque maiores dolores, consequatur molestias aliquam. Corporis error cupiditate ex ipsa? Cupiditate eum consequuntur atque illum asperiores enim porro." />
+      <Article />
       <Footer />
     </div>
   );
--- src/Article.js
+++ src/Article.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import Brooklyn from './images/blog-image-1.jpg'
+import Vogue from './images/blog-image-2.jpg'
 
-function Article({ title, imageSrc, content}) {
+function Article() {
   return (
    <article>
-    <h3>{title}</h3>
-    <img src={imageSrc} alt={`Blog Post ${title}`} />
-    <p>{content}</p>
+    <h3>"On the Street in Brooklyn"</h3>
+    <img src={Brooklyn} alt="Brooklyn"/>
+    <p>"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat, quia quo! Commodi eaque labore omnis cumque reiciendis porro ipsa aliquam delectus expedita a dolorum enim voluptatem tempore similique at nisi eos ipsam saepe, ea quasi magnam. Voluptatibus voluptatum quae, nesciunt assumenda ipsam commodi rerum enim sit, dignissimos doloremque soluta ad deserunt quis quas voluptate reiciendis laboriosam neque maiores dolores, consequatur molestias aliquam. Corporis error cupiditate ex ipsa? Cupiditate eum consequuntur atque illum asperiores enim porro."</p>
+    <a href="continues" class="continue-reading">Continues...</a>
+   
+
+   
+    <h3>"Vintage in Vogue"</h3>
+    <img src={Vogue} alt="Vogue" />
+    <p>"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat, quia quo! Commodi eaque labore omnis cumque reiciendis porro ipsa aliquam delectus expedita a dolorum enim voluptatem tempore similique at nisi eos ipsam saepe, ea quasi magnam. Voluptatibus voluptatum quae, nesciunt assumenda ipsam commodi rerum enim sit, dignissimos doloremque soluta ad deserunt quis quas voluptate reiciendis laboriosam neque maiores dolores, consequatur molestias aliquam. Corporis error cupiditate ex ipsa? Cupiditate eum consequuntur atque illum asperiores enim porro."</p>
     <a href="continues" class="continue-reading">Continues...</a>
    </article>
   )
